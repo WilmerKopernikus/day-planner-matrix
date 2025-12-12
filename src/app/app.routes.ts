dc69f17775c6b06010b22386ec617db8ba333493
Wilmer Kopernikus <wilmerkopernikus@gmail.com>
2025-12-12 16:06:21 +0800

    Create landing page with ASCII Art

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { DayPlannerComponent } from './day-planner/day-planner.component';
 import { FocusAreasComponent } from './focus-areas/focus-areas.component';
+import { LandingComponent } from './add-task/landing/landing.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'focus-areas', pathMatch: 'full' },
+  { path: '', component: LandingComponent },
   { path: 'daily-planner', component: DayPlannerComponent },
   { path: 'focus-areas', component: FocusAreasComponent },
   { path: '**', redirectTo: 'focus-areas' },
